cd8b65a6d9f5ba9afc1f81f4955e23ce7473d37e
Ifeanyi Osigwe <osigwei0930@students.bowiestate.edu>
2023-09-23 22:41:03 -0400

    Commit to my lab 2 project

---
+++ Lab 2.py
@@ -0,0 +1,7 @@
+your_name = "Ifeanyi Chibuike Osigwe"
+Major = "Information Systems and Science"
+Hobby = "Soccer","chess"
+
+print(your_name)
+print(Major)
+print(Hobby)
---
+++ Lab1.py
@@ -0,0 +1,25 @@
+import platform
+
+print('Hello World')
+print(1,2,3,4,5)
+print(2+2)
+print(10/5)
+print(1,2,3,4,5)
+print('a+b')
+name = "John"
+age = 40
+height = 5.8
+print("Name", name)
+print("Age",age)
+print("Height",height)
+
+my_name_is="Ifeanyi"
+occupation="Graduate_student"
+institution="Bowie_State_University"
+print("my_name_is",my_name_is)
+print("occupation", occupation)
+print("institution",institution)
+
+z=(20+22)
+print(z)
+
