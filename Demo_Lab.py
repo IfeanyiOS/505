bb7075dafaf2c869c6c4a27bf791ace16d4d9bb7
Ifeanyi Osigwe <osigwei0930@students.bowiestate.edu>
2023-10-02 18:07:33 -0400

    Lab 2files

---
+++ Demo_Lab.py
@@ -0,0 +1,15 @@
+name = input("What is your name:")
+age = input("What is your age:")
+major = input("What is your major:")
+institution = input("What is your institution:")
+
+# printing objects( text) in variables (name, age, major, institution)
+
+print(f"Hello {name}")
+print(f"You are {age} years old")
+print(f"your major is{major}")
+print(f"your institution is {institution}")
+
+
+
+
--- Lab 2.py
+++ Lab 2.py
@@ -1,7 +1,15 @@
+# Instructing python to print my name, major, and hobby
+
 My_name = "Ifeanyi Chibuike Osigwe"
 Major = "Information Systems and Science"
-Hobby = "Soccer","chess"
+Hobby = "Soccer"
 
 print(My_name)
 print(Major)
 print(Hobby)
+
+z="2"+"2"
+print(z)
+
+
+
