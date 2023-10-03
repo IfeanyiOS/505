833545286b976107fc9b44f60f9c8fde2681b3be
Ifeanyi Osigwe <osigwei0930@students.bowiestate.edu>
2023-10-02 21:42:22 -0400

    Lab 2files

--- Lab2partb.py
+++ Lab2partb.py
@@ -3,30 +3,30 @@ number1 = 10
 number2 = 11
 number3 = 12
 number4 = 13
-# step2
-add=number1+number2
-mult=number1*number2*number3*number4
-subtract=number4-number3-number2-number4
-division=(number4/number3)
-#step3
+# step 2
+add = number1+number2
+mult = number1*number2*number3*number4
+subtract = number4-number3-number2-number4
+division = (number4/number3)
+# step 3
 print(add,mult,subtract,division)
-#step4
+# step 4
 number1 = 13
 number2 = 12
 number3 = 11
 number4 = 10
-#step5
-add=number1+number2
-mult=number1*number2*number3*number4
-subtract=number4-number3-number2-number1
-division=(number4/number3)
-#step7
+# step 5
+add = number1+number2
+mult = number1*number2*number3*number4
+subtract = number4-number3-number2-number1
+division = (number4/number3)
+# step 7
 print(add,mult,subtract,division)
-#step8
-add=number1+number2
-mult=number3*number4
-subtract=number1-number2
-division=(number1/number2)
-#step9
+# step 8
+add = number1+number2
+mult = number3*number4
+subtract = number1-number2
+division = (number1/number2)
+# step 9
 print(add,mult,subtract,division)
 
