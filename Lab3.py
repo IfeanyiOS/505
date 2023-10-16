e2d7a380465554322c689da78674fae0850fe9cc
Ifeanyi Osigwe <osigwei0930@students.bowiestate.edu>
2023-10-16 19:23:56 -0400

    Lab 3

---
+++ Lab3.py
@@ -0,0 +1,25 @@
+# step2
+number1 = input('Enter number 1:')
+number2 = input('Enter number 2:')
+print(number1,number2)
+#step3
+sum = number1+number2
+print(sum)
+#step4
+sum = int(number1)+int(number2)
+print(sum)
+#step5
+difference = int(number1)-int(number2)
+multiply = int(number1)*int(number2)
+division = int(number1)/ int(number2)
+print(difference,multiply,division)
+#step6
+number3 = input('Enter number 3:')
+sum=int(number1)+int(number2)+int(number3)
+difference= int(number1)-int(number2)-int(number3)
+multiply = int(number1)*int(number2)*int(number3)
+division = int(number1)/int(number2)/int(number3)
+print(sum,difference,multiply,division)
+#step9
+import math
+print(math.pow(9,3))
