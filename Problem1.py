9405321016f54ef1d533b3bb1e09ffd352b95e53
Ifeanyi Osigwe <osigwei0930@students.bowiestate.edu>
2023-11-03 13:24:50 -0400

    Problem1

---
+++ Problem1.py
@@ -0,0 +1,12 @@
+"""collecting two inputs from users
+variables(length and breadth)
+"""
+length = int(input("Enter the length of a rectangle:"))
+breadth = int(input("Enter the breadth of a rectangle:"))
+""" Note: the formula to calculate the area of a rectangle is:
+area=length*breadth
+Note- values for length and breadth
+length=10, breadth= 15
+"""
+Area = length*breadth
+print(Area)
