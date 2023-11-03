c3688523aeb727c314befac8b5f3bff4d7fa5b33
Ifeanyi Osigwe <osigwei0930@students.bowiestate.edu>
2023-11-03 13:25:48 -0400

    Problem2

---
+++ Problem2.py
@@ -0,0 +1,18 @@
+# Problem 2
+"""Importing math function using import math
+to give access to perform mathematical functions
+"""
+import math
+"""using the input function to 
+insert the value for pi
+note- that the math.pi is an inbuilt function in python
+and the value for pi is constant"""
+pi = input(math.pi)
+# using the input function to input the value for radius
+radius = float(input("Enter number for radius:"))
+# radius=5
+# The formula for area of a circle; Area=pi*r**2
+Area = math.pi*radius**2
+print("Area =", Area) # final output; Area = 314.1593
+
+
